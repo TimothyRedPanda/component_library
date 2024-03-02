bbaf8a6071ec87d24d23e18b84c5278048cb4140
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-03-02 17:01:03 +0000

    Typescript

---
+++ src/components/button/button.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./button.scss";
+
+function button() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [buttonText, setButtonText] = useState("Login");
+
+	const handleClick = () => {
+		if (isLoggedIn) {
+			setIsLoggedIn(false);
+			setButtonText("Login");
+		} else {
+			setIsLoggedIn(true);
+			setButtonText("Logout");
+		}
+	};
+
+	return (
+		<div>
+			<button onClick={handleClick} type="button">
+				{buttonText}
+			</button>
+		</div>
+	);
+}
+
+export default button;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Button from "./components/button/button.tsx";
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<React.StrictMode>
+			<Button />
+		</React.StrictMode>,
+	);
+}
