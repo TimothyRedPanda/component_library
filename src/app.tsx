ce6afc5e2771b2dbcbdf0172e94de239a586bc56
Peter Carter <peter.carter@digital.justice.gov.uk>
2024-03-08 13:13:22 +0000

    add dev index

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/button";
+import { SlideButton } from "./components/slide-button";
+
+function App() {
+    return (
+        <>
+        <SlideButton></SlideButton>
+        <Button>Button</Button>
+        </>
+    )
+}
+
+export default App;
