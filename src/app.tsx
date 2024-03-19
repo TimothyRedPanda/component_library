0dbfe578d05c16e1a0abd8d75c030272a85e921d
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-03-19 18:23:17 +0000

    Card component Added

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { SlideButton, Button } from "./components";
+import { SlideButton, Button, Card } from "./components";
 import "./index.scss";
 
 function App() {
@@ -7,6 +7,18 @@ function App() {
 			<div className="wrapper">
 				<SlideButton />
 				<Button text="Click me" />
+				<Card
+					title="Red Panda"
+					description="Everything on this card is editable including the image, title and description. The card itself is a link (editable as well)."
+					image="src\assets\images\red-panda.jpg"
+					link="https://www.chewedleashphotography.com"
+				/>
+				<Card
+					title="Lemur"
+					description="Everything on this card is editable including the image, title and description. The card itself is a link (editable as well)."
+					image="src\assets\images\Lemur.jpg"
+					link="https://www.chewedleashphotography.com"
+				/>
 			</div>
 		</>
 	);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./button";
 export * from "./slide-button";
+export * from "./card";
