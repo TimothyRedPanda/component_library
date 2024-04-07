4343038297be857d5eac7f3f7cf91bc9858d4ae3
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-04-07 13:51:29 +0100

    A panda.config.json file added.

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,21 +1,33 @@
-import { motion } from "framer-motion";
-import "./button.scss";
+import { easeInOut, motion, spring, transform } from "framer-motion";
+import styled from "styled-components";
+import panda from "../../../panda.config.json";
 
-const buttonTransition = { transition: 0.25, ease: "easeInOut" };
-const buttonVariants = { y: -5, backgroundColor: "#db8758", color: "#fff" };
+const RedPandaBtn = styled(motion.button)`
+	 background-color: ${panda.primary};
+    border: 2px solid ${panda.secondary};
+    border-radius: 3rem;
+    cursor: pointer;
+    font-size: 1.5rem;
+    padding: ${panda.padding};
+    white-space: nowrap;
+`;
 
 function Button(props: { text: string; onClick: () => void }) {
 	return (
 		<>
-			<motion.button
+			<RedPandaBtn
 				onClick={props.onClick}
 				className="styledBtn"
-				whileHover={buttonVariants}
-				transition={buttonTransition}
+				whileHover={{
+					y: "-10px",
+					backgroundColor: `${panda.secondary}`,
+					color: `${panda.primary}`,
+				}}
+				transition={{ duration: 0.35, ease: easeInOut }}
 				whileTap={{ scale: 0.95 }}
 			>
 				{props.text}
-			</motion.button>
+			</RedPandaBtn>
 		</>
 	);
 }
